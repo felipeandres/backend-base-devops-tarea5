7034f513bf2bdc1e616600c4bb84f08e771a808c
Felipe Lopez Flores <felipe.lopez.flores@outlook.com>
2024-09-11 23:35:21 -0300

    se agrega sonarqube12

--- src/palindromo.ts
+++ src/palindromo.ts
@@ -2,12 +2,12 @@ export const esPalindromo = (frase: string) => {
     // Convertir la entrada a string, si no lo es
     const fraseString = String(frase);
     
-    // Eliminar espacios y convertir todo a minúsculas
+    // Eliminar espacios y convertir minusculas
     const fraseSinEspacios = fraseString.replace(/\s/g, "").toLowerCase();
     
     // Invertir la cadena
     const fraseInvertida = fraseSinEspacios.split("").reverse().join("");
     
-    // Comparar la cadena original sin espacios con su versión invertida
+    // Comparar la cadena original sin espacios con su version invertida
     return fraseSinEspacios === fraseInvertida;
 }
